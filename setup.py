9fa2b36a5cb513587b83fdf98673b98de0114ea7
konradkrasno <konradkrasno@gmail.com>
2022-02-28 12:33:53 +0100

    Add marhmallow and boto3 to requirements

--- setup.py
+++ setup.py
@@ -6,5 +6,9 @@ setup(
     package_dir={"": "."},
     packages=find_packages(where="."),
     python_requires=">=3.6",
+    install_requires=[
+        "marshmallow",
+        "boto3"
+    ],
     entry_points={"console_scripts": ["chatmaker = chat_maker.main:main"]},
 )
