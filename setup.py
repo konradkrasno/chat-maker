e49153b4d6617f16ec5e67b2746244d2ce5938fa
konradkrasno <konradkrasno@gmail.com>
2022-03-02 23:08:00 +0100

    black formatting

--- setup.py
+++ setup.py
@@ -6,9 +6,6 @@ setup(
     package_dir={"": "."},
     packages=find_packages(where="."),
     python_requires=">=3.6",
-    install_requires=[
-        "marshmallow",
-        "boto3"
-    ],
+    install_requires=["marshmallow", "boto3"],
     entry_points={"console_scripts": ["chatmaker = chat_maker.main:main"]},
 )
